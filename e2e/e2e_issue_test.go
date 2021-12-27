f9f9ab960481fb3f84cda7ecca31d5ed82a2c2ce
Sasa Prsic <sasa@ethernal.tech>
2021-12-27 13:24:24 +0100

    Add different errors on invalid nodes.

--- e2e/e2e_issue_test.go
+++ e2e/e2e_issue_test.go
@@ -3,11 +3,16 @@ package e2e
 import (
 	"errors"
 	"math"
+	"math/rand"
 	"testing"
 	"time"
 
 	"github.com/0xPolygon/pbft-consensus"
 )
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
 
 func TestE2E_Partition_MinorityCanValidate(t *testing.T) {
 	const nodesCnt = 7 // N=3F + 1, F = 2
@@ -42,21 +47,56 @@ func TestE2E_Partition_MajorityCantValidate(t *testing.T) {
 	c.WaitForHeight(3, 1*time.Minute, true, names)
 }
 
-type fsmInvalid struct {
+func TestE2E_Partition_BigMajorityCantValidate(t *testing.T) {
+	const nodesCnt = 100 // N=3F + 1, F = 2
+	hook := newPartitionTransport(300 * time.Millisecond)
+
+	c := newPBFTCluster(t, "majority_partition", "prt", nodesCnt, hook)
+	limit := int(math.Floor(nodesCnt * 2.0 / 3.0)) // + 1 removed because 2F+1 nodes is majority
+	for i, node := range c.Nodes() {
+		node.StartWithBackendFactory(createBackend(i >= limit))
+	}
+
+	names := generateNodeNames(0, limit, "prt_")
+	c.WaitForHeight(15, 2*time.Minute, false, names)
+}
+
+type fsmValidationError struct {
 	fsm
 }
 
-func (f *fsmInvalid) Validate(proposal []byte) error {
+func (f *fsmValidationError) Validate(_ []byte) error {
 	return errors.New("invalid propsal")
 }
 
+type fsmInsertError struct {
+	fsm
+}
+
+func (f *fsmInsertError) Insert(_ *pbft.SealedProposal) error {
+	return errors.New("insert error")
+}
+
+type fsmWrongHash struct {
+	fsm
+}
+
+func (f *fsmWrongHash) Hash(p []byte) []byte {
+	return []byte("")
+}
+
 func createBackend(invalid bool) func(nn *node) pbft.Backend {
-	if invalid {
-		return func(nn *node) pbft.Backend {
-			return &fsmInvalid{fsm: NewFsm(nn)}
-		}
-	}
 	return func(nn *node) pbft.Backend {
+		if invalid {
+			switch rand.Intn(3) {
+			case 0:
+				return &fsmValidationError{fsm: NewFsm(nn)}
+			case 1:
+				return &fsmInsertError{fsm: NewFsm(nn)}
+			case 2:
+				return &fsmWrongHash{fsm: NewFsm(nn)}
+			}
+		}
 		tmp := NewFsm(nn)
 		return &tmp
 	}
