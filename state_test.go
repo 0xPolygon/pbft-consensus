0a7e09890f8a773299e9303ba55f8163a0eebaaa
Stefan Negovanović <stefan@ethernal.tech>
2022-01-17 12:58:17 +0100

    Pbft consensus unit tests (msg_queue and state)

--- state_test.go
+++ state_test.go
@@ -3,17 +3,64 @@ package pbft
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
-	"crypto/rand"
+	crand "crypto/rand"
 	"fmt"
+	mrand "math/rand"
+	"reflect"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestState_FaultyNodes(t *testing.T) {
+func init() {
+	mrand.Seed(time.Now().UnixNano())
+}
+
+// Generate predefined number of validator node ids.
+// Node id is generated with a given prefixNodeId, followed by underscore and an index.
+func generateValidatorNodes(nodesCount int, prefixNodeId string) []NodeID {
+	validatorNodeIds := []NodeID{}
+	for i := 0; i < nodesCount; i++ {
+		validatorNodeIds = append(validatorNodeIds, NodeID(fmt.Sprintf("%s_%d", prefixNodeId, i)))
+	}
+	return validatorNodeIds
+}
+
+// Helper function which enables creation of MessageReq.
+func createMessage(sender string, messageType MsgType, round ...uint64) *MessageReq {
+	return createMessagePoolSender(sender, messageType, nil, round...)
+}
+
+// Helper function which enables creation of MessageReq.
+// Note: sender needs to be seeded to the pool before invoking, otherwise senderId will be set to provided sender parameter.
+func createMessagePoolSender(sender string, messageType MsgType, pool *testerAccountPool, round ...uint64) *MessageReq {
+	senderId := sender
+	if pool != nil {
+		senderId = pool.get(sender).alias
+	}
+	seal := make([]byte, 2)
+	mrand.Read(seal)
+
+	r := uint64(0)
+	if len(round) > 0 {
+		r = round[0]
+	}
+
+	msg := &MessageReq{
+		From:     NodeID(senderId),
+		Type:     messageType,
+		View:     &View{Round: r},
+		Seal:     seal,
+		Proposal: mockProposal,
+	}
+	return msg
+}
+
+func TestState_FaultyNodesCount(t *testing.T) {
 	cases := []struct {
-		Network, Faulty uint64
+		TotalNodesCount, FaultyNodesCount int
 	}{
 		{1, 0},
 		{2, 0},
@@ -24,60 +71,297 @@ func TestState_FaultyNodes(t *testing.T) {
 		{7, 2},
 		{8, 2},
 		{9, 2},
+		{10, 3},
 		{99, 32},
 		{100, 33},
 	}
 	for _, c := range cases {
-		acct := []string{}
-		for i := 0; i < int(c.Network); i++ {
-			acct = append(acct, fmt.Sprintf("acct_%d", i))
-		}
+		validatorNodeIds := generateValidatorNodes(c.TotalNodesCount, "validator")
 		s := newState()
-		s.validators = newMockValidatorSet(acct)
+		s.validators = convertToMockValidatorSet(validatorNodeIds)
 
-		assert.Equal(t, s.MaxFaultyNodes(), int(c.Faulty))
+		assert.Equal(t, s.MaxFaultyNodes(), c.FaultyNodesCount)
+	}
+}
+
+func TestState_ValidNodesCount(t *testing.T) {
+	cases := []struct {
+		TotalNodesCount, ValidNodesCount int
+	}{
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 2},
+		{5, 2},
+		{6, 2},
+		{7, 4},
+		{8, 4},
+		{9, 4},
+		{10, 6},
+		{100, 66},
+	}
+	for _, c := range cases {
+		validatorNodeIds := generateValidatorNodes(c.TotalNodesCount, "validator")
+		s := newState()
+		s.validators = convertToMockValidatorSet(validatorNodeIds)
+
+		assert.Equal(t, s.NumValid(), c.ValidNodesCount)
 	}
 }
 
 func TestState_AddMessages(t *testing.T) {
 	pool := newTesterAccountPool()
-	pool.add("A", "B", "C", "D")
+	validatorIds := []string{"A", "B", "C", "D"}
+	pool.add(validatorIds...)
 
-	c := newState()
-	c.validators = pool.validatorSet()
+	s := newState()
+	s.validators = pool.validatorSet()
 
-	msg := func(acct string, typ MsgType, round ...uint64) *MessageReq {
-		msg := &MessageReq{
-			From: NodeID(pool.get(acct).alias),
-			Type: typ,
-			View: &View{Round: 0},
-		}
-		r := uint64(0)
-		if len(round) > 0 {
-			r = round[0]
-		}
-		msg.View.Round = r
-		return msg
-	}
+	// Send message from node which is not amongst validator nodes
+	s.addMessage(createMessage("E", MessageReq_Prepare))
+	assert.Empty(t, s.committed)
+	assert.Empty(t, s.prepared)
+	assert.Empty(t, s.roundMessages)
 
 	// -- test committed messages --
-	c.addMessage(msg("A", MessageReq_Commit))
-	c.addMessage(msg("B", MessageReq_Commit))
-	c.addMessage(msg("B", MessageReq_Commit))
+	s.addMessage(createMessagePoolSender("A", MessageReq_Commit, pool))
+	s.addMessage(createMessagePoolSender("B", MessageReq_Commit, pool))
+	s.addMessage(createMessagePoolSender("B", MessageReq_Commit, pool))
 
-	assert.Equal(t, c.numCommitted(), 2)
+	assert.Equal(t, s.numCommitted(), 2)
 
 	// -- test prepare messages --
-	c.addMessage(msg("C", MessageReq_Prepare))
-	c.addMessage(msg("C", MessageReq_Prepare))
-	c.addMessage(msg("D", MessageReq_Prepare))
+	s.addMessage(createMessagePoolSender("C", MessageReq_Prepare, pool))
+	s.addMessage(createMessagePoolSender("C", MessageReq_Prepare, pool))
+	s.addMessage(createMessagePoolSender("D", MessageReq_Prepare, pool))
 
-	assert.Equal(t, c.numPrepared(), 2)
+	assert.Equal(t, s.numPrepared(), 2)
+
+	// -- test round change messages --
+	rounds := 2
+	for round := 0; round < rounds; round++ {
+		for i := 0; i < s.validators.Len(); i++ {
+			s.addMessage(createMessagePoolSender(validatorIds[i], MessageReq_RoundChange, pool, uint64(round)))
+		}
+	}
+
+	for round := 0; round < rounds; round++ {
+		assert.Equal(t, rounds, len(s.roundMessages))
+		msgsPerRound := s.roundMessages[uint64(round)]
+		assert.Equal(t, s.validators.Len(), len(msgsPerRound))
+	}
 }
 
+func TestState_MaxRound_Found(t *testing.T) {
+	validatorsCount := 5
+	roundsCount := 6
+	s := newState()
+
+	validatorIds := make([]string, validatorsCount)
+	for i := 0; i < validatorsCount; i++ {
+		validatorId := fmt.Sprintf("validator_%d", i)
+		validatorIds[i] = validatorId
+	}
+	s.validators = newMockValidatorSet(validatorIds)
+
+	for round := 0; round < roundsCount; round++ {
+		if round%2 == 1 {
+			for _, validatorId := range validatorIds {
+				s.addMessage(createMessage(validatorId, MessageReq_RoundChange, uint64(round)))
+			}
+		} else {
+			s.addMessage(createMessage(validatorIds[mrand.Intn(validatorsCount)], MessageReq_RoundChange, uint64(round)))
+		}
+	}
+
+	maxRound, found := s.maxRound()
+	assert.Equal(t, uint64(5), maxRound)
+	assert.Equal(t, true, found)
+}
+
+func TestState_MaxRound_NotFound(t *testing.T) {
+	validatorsCount := 7
+	s := newState()
+
+	validatorIds := make([]string, validatorsCount)
+	for i := 0; i < validatorsCount; i++ {
+		validatorIds[i] = fmt.Sprintf("validator_%d", i)
+	}
+	s.validators = newMockValidatorSet(validatorIds)
+
+	// Send wrong message type from some validator, whereas roundMessages map is empty
+	s.addMessage(createMessage(validatorIds[0], MessageReq_Preprepare))
+
+	maxRound, found := s.maxRound()
+	assert.Equal(t, maxRound, uint64(0))
+	assert.Equal(t, found, false)
+
+	// Seed insufficient "RoundChange" messages count, so that maxRound isn't going to be found
+	for round := range validatorIds {
+		if round%2 == 0 {
+			// Each even round should populate more than one "RoundChange" messages, but just enough that we don't reach census (max faulty nodes+1)
+			for i := 0; i < s.MaxFaultyNodes(); i++ {
+				s.addMessage(createMessage(validatorIds[mrand.Intn(validatorsCount)], MessageReq_RoundChange, uint64(round)))
+			}
+		} else {
+			s.addMessage(createMessage(validatorIds[mrand.Intn(validatorsCount)], MessageReq_RoundChange, uint64(round)))
+		}
+	}
+
+	maxRound, found = s.maxRound()
+	assert.Equal(t, uint64(0), maxRound)
+	assert.Equal(t, false, found)
+}
+
+func TestState_AddRoundMessage(t *testing.T) {
+	s := newState()
+	validatorIds := []string{"validator_1", "validator_2"}
+	s.validators = newMockValidatorSet(validatorIds)
+
+	roundMessageSize := s.AddRoundMessage(createMessage(validatorIds[0], MessageReq_Commit, 0))
+	assert.Equal(t, 0, roundMessageSize)
+	assert.Equal(t, 0, len(s.roundMessages))
+
+	s.AddRoundMessage(createMessage(validatorIds[0], MessageReq_RoundChange, 0))
+	s.AddRoundMessage(createMessage(validatorIds[0], MessageReq_RoundChange, 1))
+	s.AddRoundMessage(createMessage(validatorIds[0], MessageReq_RoundChange, 2))
+
+	roundMessageSize = s.AddRoundMessage(createMessage(validatorIds[1], MessageReq_RoundChange, 2))
+	assert.Equal(t, 2, roundMessageSize)
+
+	s.AddRoundMessage(createMessage(validatorIds[1], MessageReq_RoundChange, 3))
+	assert.Equal(t, 4, len(s.roundMessages))
+
+	assert.Empty(t, s.prepared)
+	assert.Empty(t, s.committed)
+}
+
+func TestState_addPrepared(t *testing.T) {
+	s := newState()
+	validatorIds := []string{"validator_1", "validator_2"}
+	s.validators = newMockValidatorSet(validatorIds)
+
+	s.addPrepared(createMessage(validatorIds[0], MessageReq_Commit))
+	assert.Equal(t, 0, len(s.prepared))
+
+	s.addPrepared(createMessage(validatorIds[0], MessageReq_Prepare))
+	s.addPrepared(createMessage(validatorIds[1], MessageReq_Prepare))
+
+	assert.Equal(t, len(validatorIds), len(s.prepared))
+	assert.Empty(t, s.committed)
+	assert.Empty(t, s.roundMessages)
+}
+
+func TestState_addCommitted(t *testing.T) {
+	s := newState()
+	validatorIds := []string{"validator_1", "validator_2"}
+	s.validators = newMockValidatorSet(validatorIds)
+
+	s.addCommitted(createMessage(validatorIds[0], MessageReq_Prepare))
+	assert.Empty(t, 0, s.committed)
+
+	s.addCommitted(createMessage(validatorIds[0], MessageReq_Commit))
+	s.addCommitted(createMessage(validatorIds[1], MessageReq_Commit))
+
+	assert.Equal(t, len(validatorIds), len(s.committed))
+	assert.Empty(t, s.prepared)
+	assert.Empty(t, s.roundMessages)
+}
+
+func TestState_Copy(t *testing.T) {
+	originalMsg := createMessage("validator_1", MessageReq_Preprepare, 0)
+	copyMsg := originalMsg.Copy()
+	assert.NotSame(t, originalMsg, copyMsg)
+	assert.Equal(t, originalMsg, copyMsg)
+}
+
+func TestState_Lock_Unlock(t *testing.T) {
+	s := newState()
+	proposalData := make([]byte, 2)
+	mrand.Read(proposalData)
+	s.proposal = &Proposal{
+		Data: proposalData,
+		Time: time.Now(),
+	}
+	s.lock()
+	assert.True(t, s.locked)
+	assert.NotNil(t, s.proposal)
+
+	s.unlock()
+	assert.False(t, s.locked)
+	assert.Nil(t, s.proposal)
+}
+
+func TestState_GetSequence(t *testing.T) {
+	s := newState()
+	s.view = &View{Sequence: 3, Round: 0}
+	assert.True(t, s.GetSequence() == 3)
+}
+
+func TestState_getCommittedSeals(t *testing.T) {
+	s := newState()
+	validatorIds := []string{"A", "B"}
+	s.validators = newMockValidatorSet(validatorIds)
+
+	s.addCommitted(createMessage(validatorIds[0], MessageReq_Commit))
+	s.addCommitted(createMessage(validatorIds[1], MessageReq_Commit))
+	committedSeals := s.getCommittedSeals()
+
+	assert.Len(t, committedSeals, 2)
+	assert.True(t, &committedSeals[0] != &s.committed["A"].Seal)
+	foundSeal := false
+	for _, seal := range committedSeals {
+		if reflect.DeepEqual(seal, s.committed["A"].Seal) {
+			foundSeal = true
+		}
+	}
+	assert.True(t, &committedSeals[0] != &s.committed["A"].Seal)
+	assert.True(t, foundSeal)
+
+	foundSeal = false
+	for _, seal := range committedSeals {
+		if reflect.DeepEqual(seal, s.committed["B"].Seal) {
+			foundSeal = true
+		}
+	}
+
+	assert.True(t, &committedSeals[1] != &s.committed["B"].Seal)
+	assert.True(t, foundSeal)
+}
+
+func TestMsgType_ToString(t *testing.T) {
+	expectedMapping := map[MsgType]string{
+		MessageReq_RoundChange: "RoundChange",
+		MessageReq_Preprepare:  "Preprepare",
+		MessageReq_Commit:      "Commit",
+		MessageReq_Prepare:     "Prepare",
+	}
+
+	for msgType, expected := range expectedMapping {
+		assert.Equal(t, expected, msgType.String())
+	}
+}
+
+func TestPbftState_ToString(t *testing.T) {
+	expectedMapping := map[PbftState]string{
+		AcceptState:      "AcceptState",
+		RoundChangeState: "RoundChangeState",
+		ValidateState:    "ValidateState",
+		CommitState:      "CommitState",
+		SyncState:        "SyncState",
+		DoneState:        "DoneState",
+	}
+
+	for pbftState, expected := range expectedMapping {
+		assert.Equal(t, expected, pbftState.String())
+	}
+}
+
+type signDelegate func([]byte) ([]byte, error)
 type testerAccount struct {
-	alias string
-	priv  *ecdsa.PrivateKey
+	alias  string
+	priv   *ecdsa.PrivateKey
+	signFn signDelegate
 }
 
 func (t *testerAccount) NodeID() NodeID {
@@ -85,7 +369,9 @@ func (t *testerAccount) NodeID() NodeID {
 }
 
 func (t *testerAccount) Sign(b []byte) ([]byte, error) {
-	// TODO:
+	if t.signFn != nil {
+		return t.signFn(b)
+	}
 	return nil, nil
 }
 
@@ -121,37 +407,41 @@ func (ap *testerAccountPool) add(accounts ...string) {
 }
 
 func (ap *testerAccountPool) get(name string) *testerAccount {
-	for _, i := range ap.accounts {
-		if i.alias == name {
-			return i
+	for _, account := range ap.accounts {
+		if account.alias == name {
+			return account
 		}
 	}
 	return nil
 }
 
 func (ap *testerAccountPool) validatorSet() ValidatorSet {
-	acct := []string{}
-	for _, i := range ap.accounts {
-		acct = append(acct, i.alias)
+	validatorIds := []NodeID{}
+	for _, acc := range ap.accounts {
+		validatorIds = append(validatorIds, NodeID(acc.alias))
 	}
-	return newMockValidatorSet(acct)
+	return convertToMockValidatorSet(validatorIds)
 }
 
 func generateKey() *ecdsa.PrivateKey {
-	prv, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	prv, err := ecdsa.GenerateKey(elliptic.P384(), crand.Reader)
 	if err != nil {
 		panic(err)
 	}
 	return prv
 }
 
-func newMockValidatorSet(a []string) ValidatorSet {
-	valsAsNode := []NodeID{}
-	for _, i := range a {
-		valsAsNode = append(valsAsNode, NodeID(i))
+func newMockValidatorSet(validatorIds []string) ValidatorSet {
+	validatorNodeIds := []NodeID{}
+	for _, id := range validatorIds {
+		validatorNodeIds = append(validatorNodeIds, NodeID(id))
 	}
-	set := valString(valsAsNode)
-	return &set
+	return convertToMockValidatorSet(validatorNodeIds)
+}
+
+func convertToMockValidatorSet(validatorIds []NodeID) ValidatorSet {
+	validatorSet := valString(validatorIds)
+	return &validatorSet
 }
 
 type valString []NodeID
@@ -168,10 +458,10 @@ func (v *valString) CalcProposer(round uint64) NodeID {
 	return (*v)[pick]
 }
 
-func (v *valString) Index(addr NodeID) int {
-	for indx, i := range *v {
-		if i == addr {
-			return indx
+func (v *valString) Index(id NodeID) int {
+	for i, currentId := range *v {
+		if currentId == id {
+			return i
 		}
 	}
 
@@ -179,8 +469,8 @@ func (v *valString) Index(addr NodeID) int {
 }
 
 func (v *valString) Includes(id NodeID) bool {
-	for _, i := range *v {
-		if i == id {
+	for _, currentId := range *v {
+		if currentId == id {
 			return true
 		}
 	}
