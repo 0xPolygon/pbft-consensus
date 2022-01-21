86ffa11c77fb09a9e67b8466d57dbbe53863cd73
Saša Pršić <93726535+0xSasaPrsic@users.noreply.github.com>
2022-01-21 11:35:36 +0100

    POS-130 - Fuzz tests (#17)
    * Add fuzz tests.

--- e2e/helper.go
+++ e2e/helper.go
@@ -1,6 +1,11 @@
 package e2e
 
-import "strconv"
+import (
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
 
 func generateNodeNames(from int, count int, prefix string) []string {
 	var names []string
@@ -8,4 +13,42 @@ func generateNodeNames(from int, count int, prefix string) []string {
 		names = append(names, prefix+strconv.Itoa(j))
 	}
 	return names
+}
+
+func executeInTimerAndWait(tickTime time.Duration, duration time.Duration, fn func(time.Duration)) {
+	end := executeInTimer(tickTime, duration, fn)
+	<-end
+}
+
+func executeInTimer(tickTime time.Duration, duration time.Duration, fn func(time.Duration)) chan struct{} {
+	tick := time.NewTicker(tickTime)
+	tickerDone := make(chan struct{})
+	end := make(chan struct{})
+	startTime := time.Now()
+	go func() {
+		for {
+			select {
+			case v := <-tick.C:
+				elapsedTime := v.Sub(startTime)
+				fn(elapsedTime)
+			case <-tickerDone:
+				close(end)
+				return
+			}
+		}
+	}()
+
+	after := time.After(duration)
+	go func() {
+		<-after
+		tick.Stop()
+		close(tickerDone)
+	}()
+	return end
+}
+
+func isFuzzEnabled(t *testing.T) {
+	if os.Getenv("FUZZ") != "true" {
+		t.Skip("Fuzz tests are disabled.")
+	}
 }
