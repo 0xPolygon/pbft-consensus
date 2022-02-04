1513d26e1b7065efb9dc3ea7205c3a70424e417d
Stefan Negovanović <93934272+Stefan-Ethernal@users.noreply.github.com>
2022-02-04 06:43:55 +0100

    Implement liveness issue test proof (#22)
    * Implement integration test that has issue with locking different proposals in 2 different rounds.
    
    * Various code cleanup and refactorings
    
    * Reverted assertions made in liveness test, moving round routing messages to genericGossipTransport struct
    
    Co-authored-by: Sasa Prsic <sasa@ethernal.tech>

--- e2e/transport.go
+++ e2e/transport.go
@@ -65,6 +65,91 @@ type transportHook interface {
 	GetPartitions() map[string][]string
 }
 
+type sender pbft.NodeID
+type receivers []pbft.NodeID
+
+// Encapsulates message routing for certain round
+type roundMetadata struct {
+	round      uint64
+	routingMap map[sender]receivers
+}
+
+// Callback which enables determining which message should be gossiped
+type gossipHandler func(sender, receiver pbft.NodeID, msg *pbft.MessageReq) bool
+
+// Transport implementation which enables specifying custom gossiping logic
+type genericGossipTransport struct {
+	flowMap       map[uint64]roundMetadata
+	gossipHandler gossipHandler
+}
+
+// Initialize new generic gossip transport
+func newGenericGossipTransport() *genericGossipTransport {
+	defaultGossipHandler := func(sender, receiver pbft.NodeID, msg *pbft.MessageReq) bool {
+		return true
+	}
+	return &genericGossipTransport{
+		flowMap:       make(map[uint64]roundMetadata),
+		gossipHandler: defaultGossipHandler,
+	}
+}
+
+// Function which attaches gossip handler
+func (t *genericGossipTransport) withGossipHandler(gossipHandler gossipHandler) *genericGossipTransport {
+	t.gossipHandler = gossipHandler
+	return t
+}
+
+// Function which sets message routing per round mapping
+func (t *genericGossipTransport) withFlowMap(flowMap map[uint64]roundMetadata) *genericGossipTransport {
+	t.flowMap = flowMap
+	return t
+}
+
+// Function determining whether a message should be gossiped, based on provided flow map, which describes messages routing per round.
+func (t *genericGossipTransport) shouldGossipBasedOnMsgFlowMap(msg *pbft.MessageReq, senderId pbft.NodeID, receiverId pbft.NodeID) bool {
+	roundMedatada, ok := t.flowMap[msg.View.Round]
+	if !ok {
+		return false
+	}
+
+	if roundMedatada.round == msg.View.Round {
+		receivers, ok := roundMedatada.routingMap[sender(senderId)]
+		if !ok {
+			return false
+		}
+
+		foundReceiver := false
+		for _, v := range receivers {
+			if v == receiverId {
+				foundReceiver = true
+				break
+			}
+		}
+		return foundReceiver
+	}
+	return true
+}
+
+func (t *genericGossipTransport) Gossip(from, to pbft.NodeID, msg *pbft.MessageReq) bool {
+	if t.gossipHandler != nil {
+		return t.gossipHandler(from, to, msg)
+	}
+	return true
+}
+
+func (t *genericGossipTransport) Connects(from, to pbft.NodeID) bool {
+	return true
+}
+
+func (t *genericGossipTransport) Reset() {
+	t.gossipHandler = nil
+}
+
+func (t *genericGossipTransport) GetPartitions() map[string][]string {
+	return nil
+}
+
 // latency transport
 type randomTransport struct {
 	jitterMax time.Duration
@@ -86,6 +171,7 @@ func (r *randomTransport) Gossip(from, to pbft.NodeID, msg *pbft.MessageReq) boo
 	}
 	return true
 }
+
 func (r *randomTransport) Reset() {
 	// no impl
 }
