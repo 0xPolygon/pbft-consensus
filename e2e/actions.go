c1053ff210828972339aceb4897a876b55561c48
Stefan Negovanović <stefan@ethernal.tech>
2022-02-10 12:29:25 +0100

    Add Validate method to Action. Add GetFilteredNodes method to the Cluster. Implement node drop logic.

--- e2e/actions.go
+++ e2e/actions.go
@@ -1,8 +1,12 @@
 package e2e
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"time"
+
+	"github.com/0xPolygon/pbft-consensus"
 )
 
 type Scenario struct {
@@ -28,36 +32,66 @@ func (s *Scenario) CleanUp(cluster *Cluster) {
 
 type Action interface {
 	// TODO: Add Setup() method?
+	Validate(c *Cluster) error
 	Apply(c *Cluster)
 	Revert(c *Cluster)
 }
 
 // This action encapsulates logic for dropping nodes action.
 type DropNodeAction struct {
-	nodesCount       int
 	nodesToDrop      int
 	droppingInterval time.Duration
 	duration         time.Duration
 }
 
-func NewDropNodeAction(nodeCount, nodesToDrop int, droppingInterval, duration time.Duration) *DropNodeAction {
+func NewDropNodeAction(nodesToDrop int, droppingInterval, duration time.Duration) *DropNodeAction {
 	return &DropNodeAction{
-		nodesCount:       nodeCount,
 		nodesToDrop:      nodesToDrop,
 		droppingInterval: droppingInterval,
 		duration:         duration,
 	}
 }
 
+func (dn *DropNodeAction) Validate(c *Cluster) error {
+	nodesCount := len(c.nodes)
+	if dn.nodesToDrop >= nodesCount {
+		return fmt.Errorf("trying to drop more nodes (%d) than available in the cluster (%d)", dn.nodesToDrop, nodesCount)
+	}
+	runningNodes := c.GetRunningNodes()
+	nodesLeft := len(runningNodes) - dn.nodesToDrop
+	if nodesLeft < pbft.MaxFaultyNodes(nodesCount) {
+		return fmt.Errorf("dropping %d nodes would jeopardize Byzantine fault tollerancy.\nExpected at least %d nodes to run, but action would leave it to %d",
+			dn.nodesToDrop, pbft.MaxFaultyNodes(nodesCount), nodesLeft)
+	}
+	return nil
+}
+
 func (dn *DropNodeAction) Apply(c *Cluster) {
+	if err := dn.Validate(c); err != nil {
+		log.Printf("[WARNING] Skipping drop node action. Reason: '%s'", err)
+		return
+	}
+
 	ticker := time.NewTicker(dn.droppingInterval)
 	after := time.After(dn.duration)
 	for {
 		select {
 		case <-ticker.C:
-			// TODO: Implement node dropping logic here with some probability
-			// cluster.StopNode()
-			log.Println("Dropping node.")
+			var runningNodes []*node
+			for i := 0; i < dn.nodesToDrop; i++ {
+				log.Printf("Running nodes: %v", runningNodes)
+				runningNodes = c.GetRunningNodes()
+				if len(runningNodes) == 0 {
+					ticker.Stop()
+					return
+				}
+				// Add probability to stop a node
+				if rand.Intn(100) > 50 {
+					nodeToStop := runningNodes[rand.Intn(len(runningNodes))]
+					log.Printf("Dropping node '%s'.", nodeToStop.name)
+					c.StopNode(nodeToStop.name)
+				}
+			}
 		case <-after:
 			ticker.Stop()
 			log.Println("Done dropping nodes.")
