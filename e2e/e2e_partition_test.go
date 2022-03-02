ce492ad5a77b85b653a77f6da417f60933b95844
goran-ethernal <100121253+goran-ethernal@users.noreply.github.com>
2022-03-02 09:25:59 +0100

    POS-225 Liveness issue test case 2 (#31)
    * Implement integration test that has issue with locking different proposals in 2 different rounds.
    
    * Implementarion of liveness issue case 2.
    
    * Liveness issue case 2 minor changes
    
    * Liveness issue case 2 test - additional changes
    
    * Liveness issue case 2 - removed unnecessary logs and comments
    
    Co-authored-by: Sasa Prsic <sasa@ethernal.tech>

--- e2e/e2e_partition_test.go
+++ e2e/e2e_partition_test.go
@@ -20,7 +20,7 @@ import (
 // This test creates a single cluster of 5 nodes, but instead of letting all the peers communicate with each other,
 // it routes messages only to specific nodes and induces that nodes lock on different proposal.
 // In the round=1, it marks one node as faulty (meaning that it doesn't takes part in gossiping).
-func TestE2E_Partition_Liveness(t *testing.T) {
+func TestE2E_Partition_LivenessIssue_Case1_FiveNodes_OneFaulty(t *testing.T) {
 	const nodesCnt = 5
 	round0 := roundMetadata{
 		round: 0,
@@ -85,6 +85,94 @@ func TestE2E_Partition_Liveness(t *testing.T) {
 	// log to check what is the end state
 	for _, n := range c.nodes {
 		t.Logf("Node %v, isProposalLocked: %v, proposal data: %v\n", n.name, n.pbft.IsLocked(), n.pbft.GetProposal().Data)
+	}
+
+	assert.NoError(t, err)
+}
+
+// Test proves existence of liveness issues which is described in
+// Correctness Analysis of Istanbul Byzantine Fault Tolerance(https://arxiv.org/pdf/1901.07160.pdf).
+// Specific problem this test is emulating is described in Chapter 7.1, Case 2.
+// Summary of the problem is that there are not enough nodes to lock on single proposal,
+// due to some issues where nodes being unable to deliver messages to all of the peers.
+// When there are (nh) = 6 nodes, and they are split into three subsets, all locked on different proposals, where one of the nodes becomes faulty,
+// nodes are unable to reach a consensus on a single proposal, resulting in continuous RoundChange.
+// This test creates a single cluster of 6 nodes, but instead of letting all the peers communicate with each other,
+// it routes messages only to specific nodes and induces that nodes lock on different proposal.
+// In the round=3, it marks one node as faulty (meaning that it doesn't takes part in gossiping).
+func TestE2E_Partition_LivenessIssue_Case2_SixNodes_OneFaulty(t *testing.T) {
+	const nodesCnt = 6
+	round0 := roundMetadata{
+		round: 0,
+		// lock A_1, A_4
+		routingMap: map[sender]receivers{
+			"A_0": {"A_1", "A_3", "A_4"},
+			"A_3": {"A_1", "A_3", "A_4"},
+			"A_4": {"A_1", "A_4"},
+		},
+	}
+
+	round2 := roundMetadata{
+		round: 2,
+		// lock A_5
+		routingMap: map[sender]receivers{
+			"A_0": {"A_5", "A_2", "A_4"},
+			"A_1": {"A_5", "A_0"},
+			"A_2": {"A_5", "A_3"},
+			"A_3": {"A_5"},
+			"A_4": {"A_5"},
+		},
+	}
+
+	round3 := roundMetadata{
+		round: 3,
+		// lock A_3 and A_0 on one proposal and A_2 will be faulty
+		routingMap: map[sender]receivers{
+			"A_3": {"A_0", "A_2", "A_3", "A_4"},
+			"A_0": {"A_0", "A_3", "A_4"},
+			"A_2": {"A_0", "A_1", "A_3", "A_4"},
+		},
+	}
+	flowMap := map[uint64]roundMetadata{0: round0, 2: round2, 3: round3}
+	transport := newGenericGossipTransport()
+	faultyNodeId := pbft.NodeID("A_2")
+
+	// If livenessGossipHandler returns false, message should not be transported.
+	livenessGossipHandler := func(senderId, receiverId pbft.NodeID, msg *pbft.MessageReq) (sent bool) {
+		if msg.View.Round == 1 && msg.Type == pbft.MessageReq_RoundChange {
+			return true
+		}
+
+		if msg.View.Round > 3 || msg.View.Sequence > 2 {
+			// Faulty node is unresponsive after round 3, and all the other nodes are gossiping all the messages.
+			return senderId != faultyNodeId && receiverId != faultyNodeId
+		} else {
+			if msg.View.Round <= 1 && msg.Type == pbft.MessageReq_Commit {
+				// Cut all the commit messages gossiping for round 0 and 1
+				return false
+			}
+			if msg.View.Round == 3 && senderId == faultyNodeId &&
+				(msg.Type == pbft.MessageReq_RoundChange || msg.Type == pbft.MessageReq_Commit) {
+				// Case where we are in round 3 and 2 different nodes will lock the proposal
+				// (consequence of faulty node doesn't gossip round change and commit messages).
+				return false
+			}
+		}
+
+		return transport.shouldGossipBasedOnMsgFlowMap(msg, senderId, receiverId)
+	}
+
+	transport.withFlowMap(flowMap).withGossipHandler(livenessGossipHandler)
+
+	c := newPBFTCluster(t, "liveness_issue", "A", nodesCnt, transport)
+	c.Start()
+	defer c.Stop()
+
+	err := c.WaitForHeight(3, 1*time.Minute)
+
+	// log to check what is the end state
+	for _, n := range c.nodes {
+		t.Logf("Node %v, isProposalLocked: %v, proposal data: %v\n", n.name, n.pbft.IsStateLocked(), n.pbft.GetProposal().Data)
 	}
 
 	assert.NoError(t, err)
