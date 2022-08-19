samkenxstream/turnkey-triumph-326606_pbft-consensus

Liveness fix (introduce relocking concept)

state.go
@@ -47,7 +47,7 @@ type MessageReq struct {
 	// hash of the proposal
 	Hash []byte
 
-	// proposal is the arbitrary data proposal (only for preprepare messages)
+	// proposal is the arbitrary data proposal (relayed only for preprepare messages)
 	Proposal []byte
 }
 
@@ -192,8 +192,9 @@ type currentState struct {
 	// List of round change messages
 	roundMessages map[uint64]map[NodeID]*MessageReq
 
-	// Locked signals whether the proposal is locked
-	locked bool
+	// Locked round signals whether the proposal is locked and in which round.
+	// If it is nil, it means that proposal isn't locked.
+	lockedRound *uint64
 
 	// Describes whether there has been an error during the computation
 	err error
@@ -208,7 +209,16 @@ func newState() *currentState {
 }
 
 func (c *currentState) IsLocked() bool {
-	return c.locked
+	return c.lockedRound != nil
+}
+
+func (c *currentState) lock(round uint64) {
+	c.lockedRound = &round
+}
+
+func (c *currentState) unlock() {
+	c.proposal = nil
+	c.lockedRound = nil
 }
 
 func (c *currentState) GetSequence() uint64 {
@@ -283,15 +293,6 @@ func (c *currentState) resetRoundMsgs() {
 // CalcProposer calculates the proposer and sets it to the state
 func (c *currentState) CalcProposer() {
 	c.proposer = c.validators.CalcProposer(c.view.Round)
-}
-
-func (c *currentState) lock() {
-	c.locked = true
-}
-
-func (c *currentState) unlock() {
-	c.proposal = nil
-	c.locked = false
 }
 
 // cleanRound deletes the specific round messages

0xPolygon/pbft-consensus

state.go
@@ -31,8 +31,9 @@ type state struct {
 	// List of round change messages
 	roundMessages map[uint64]map[NodeID]*MessageReq
 
-	// Locked signals whether the proposal is locked
-	locked uint64
+	// Locked round signals whether the proposal is locked and in which round.
+	// If it is nil, it means that proposal isn't locked.
+	lockedRound *uint64
 
 	// timeout tracks the time left for this round
 	timeoutChan <-chan time.Time
@@ -55,7 +56,7 @@ func newState() *state {
 }
 
 func (s *state) IsLocked() bool {
-	return atomic.LoadUint64(&s.locked) == 1
+	return s.lockedRound != nil
 }
 
 func (s *state) GetSequence() uint64 {
@@ -134,14 +135,13 @@ func (s *state) CalcProposer() {
 	s.proposer = s.validators.CalcProposer(s.view.Round)
 }
 
-func (s *state) lock() {
-	atomic.StoreUint64(&s.locked, 1)
+func (s *state) lock(round uint64) {
+	s.lockedRound = &round
 }
 
 func (s *state) unlock() {
 	s.proposal = nil
-
-	atomic.StoreUint64(&s.locked, 0)
+	s.lockedRound = nil
 }
 
 // cleanRound deletes the specific round messages